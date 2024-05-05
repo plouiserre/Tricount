c23aeea91343f1479b05586b4994627bf5f9ff05
Pierre-Louis Serré <pierre-louisserre@airdepirrelouis.home>
2024-05-05 16:51:35 +0200

    init project

---
+++ src/test/java/com/plouiserre/tricount/TricountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plouiserre.tricount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
